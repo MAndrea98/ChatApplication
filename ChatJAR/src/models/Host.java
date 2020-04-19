2a8ae3cc7fb523c2fbf70d00254fec87be0384e6
MAndrea98 <mendrei.andrea@gmail.com>
2020-04-19 18:37:25 +0200

    The models of the project are done

---
+++ ChatJAR/src/models/Host.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Host {
+
+	private String alias;
+	private String address;
+	
+	public Host() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Host(String alias, String address) {
+		super();
+		this.alias = alias;
+		this.address = address;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
---
+++ ChatJAR/src/models/Message.java
@@ -0,0 +1,68 @@
+package models;
+
+import java.util.Calendar;
+
+public class Message {
+
+	private User sender;
+	private User reciever;
+	private Calendar date;
+	private String subject;
+	private String text;
+	
+	public Message() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Message(User sender, User reciever, Calendar date, String subject, String text) {
+		super();
+		this.sender = sender;
+		this.reciever = reciever;
+		this.date = date;
+		this.subject = subject;
+		this.text = text;
+	}
+
+	public User getSender() {
+		return sender;
+	}
+
+	public void setSender(User sender) {
+		this.sender = sender;
+	}
+
+	public User getReciever() {
+		return reciever;
+	}
+
+	public void setReciever(User reciever) {
+		this.reciever = reciever;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
